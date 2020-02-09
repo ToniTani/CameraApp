c1f86a06d1d11a56fe3a94544c1737dc09542042
Toni <toni.tani@student.saimia.fi>
2020-02-09 22:53:16 +0200

    Initial Commit

---
+++ CameraApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraApp
+//
+//  Created by Toni Tani on 9.2.2020.
+//  Copyright © 2020 Toni Tani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
