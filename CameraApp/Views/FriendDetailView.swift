395eb7f1eaa979450205812fc527be56620ec062
Toni <toni.tani@student.saimia.fi>
2020-02-09 23:09:41 +0200

    Added project pictures into assets.

---
+++ CameraApp/Views/FriendDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  FriendDetailView.swift
+//  CameraApp
+//
+//  Created by Toni Tani on 9.2.2020.
+//  Copyright © 2020 Toni Tani. All rights reserved.
+//
+
+import SwiftUI
+
+struct FriendDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendDetailView()
+    }
+}
---
+++ CameraApp/Views/FriendsView.swift
@@ -0,0 +1,21 @@
+//
+//  FriendsView.swift
+//  CameraApp
+//
+//  Created by Toni Tani on 9.2.2020.
+//  Copyright © 2020 Toni Tani. All rights reserved.
+//
+
+import SwiftUI
+
+struct FriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendsView()
+    }
+}
